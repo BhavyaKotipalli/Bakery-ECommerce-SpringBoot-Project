5061b5bd2e9f4d975080409e6a35f71e25caa415
Bhavya <bhavyasrisai535@gmail.com>
2025-12-27 23:33:13 +0530

    Initial commit of Bakery ECommerce SpringBoot Project

---
+++ src/main/java/com/project/entity/Category.java
@@ -0,0 +1,32 @@
+package com.project.entity;
+
+import jakarta.persistence.*;
+@Entity
+@Table
+public class Category {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	@Override
+	public String toString() {
+		return "Category [id=" + id + ", name=" + name + "]";
+	}
+	public Category(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	private String name;
+}
